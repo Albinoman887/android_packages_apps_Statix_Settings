33571a8155a453795eb430474f44f62c1ae24599
Matt Filetto <matt.filetto@gmail.com>
2023-01-06 10:44:39 -0800

    remove unused features
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- src/com/statix/android/settings/StatixSettingsActivity.java
+++ src/com/statix/android/settings/StatixSettingsActivity.java
@@ -4,14 +4,14 @@ import com.android.settings.SettingsActivity;
 
 public class StatixSettingsActivity extends SettingsActivity {
 
-    @Override
-    protected boolean isValidFragment(String fragmentName) {
-        // Almost all fragments are wrapped in this,
+//    @Override
+ //   protected boolean isValidFragment(String fragmentName) {
+//        // Almost all fragments are wrapped in this,
         // except for a few that have their own activities.
-        for (int i = 0; i < StatixSettingsGateway.ENTRY_FRAGMENTS.length; i++) {
-            if (StatixSettingsGateway.ENTRY_FRAGMENTS[i].equals(fragmentName)) return true;
-        }
-        return false;
-    }
+ //       for (int i = 0; i < StatixSettingsGateway.ENTRY_FRAGMENTS.length; i++) {
+  //          if (StatixSettingsGateway.ENTRY_FRAGMENTS[i].equals(fragmentName)) return true;
+ //       }
+ //       return false;
+ //   }
 
 }
--- src/com/statix/android/settings/StatixSettingsGateway.java
+++ src/com/statix/android/settings/StatixSettingsGateway.java
@@ -8,7 +8,7 @@ public class StatixSettingsGateway {
      * A list of fragment that can be hosted by StatixSettingsActivity. SettingsActivity will throw a
      * security exception if the fragment it needs to display is not in this list.
      */
-    public static final String[] ENTRY_FRAGMENTS = {
-        SmartPixels.class.getName(),
-    };
+  //  public static final String[] ENTRY_FRAGMENTS = {
+   //     SmartPixels.class.getName(),
+   // };
 }
